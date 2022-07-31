b8abf03ea2d9dec5ee25e48a40c06c34c53ceacc
CheoR <cheo.roman+github@gmail.com>
2022-07-31 06:40:24 -0500

    build: render mdx content

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,6 @@ module.exports = {
         path: `${__dirname}/blog`,
       },
     },
+    "gatsby-plugin-mdx",
   ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,28 +1,36 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
 
 const Blog = ({ data }) => {
   console.log("-------- data --");
-  console.log(data.allFile.nodes);
+  console.log(data.allMdx.nodes);
   console.log("-------- data --");
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.datePublished}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { fields: frontmatter___datePublished, order: DESC }) {
       nodes {
         id
-        name
+        body
+        frontmatter {
+          author
+          datePublished(formatString: "MMMM D, YYYY")
+          title
+        }
       }
     }
   }
