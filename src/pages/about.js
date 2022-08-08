e2cfb41ab85e6c8f1c2e9c472f9c79975f05b153
CheoR <cheo.roman+github@gmail.com>
2022-08-08 07:34:32 -0500

    config: config formspree contact form

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,46 @@ import Layout from "../components/layout";
 const AboutPage = () => {
   return (
     <Layout pageTitle="About">
-      <p>I'm a little piggy.</p>
+      <section className="main__content">
+        <p>
+          <b>Hi</b> My name is Cheo. I'm a Nashville-based developer.
+        </p>
+        <p>
+          I have been programming as a hobby on/off for years. I started with
+          C++ in highschool and dabbled with a bit of other languages since
+          then. In college, I used Prolog for a computational linguistics course
+          where we{" "}
+          <a
+            href="https://news.ucsc.edu/2008/06/2278.html"
+            aria-label="University of California - Santa Cruz Computational Linguistics Speech-Operated Roboic Cars"
+          >
+            drove robotic cars using our voice.
+          </a>
+        </p>
+        <p>
+          Due to health reasons after graduating, I could no longer do Peace
+          Corp on medical grounds, so I continued learning at a local community
+          college and state school. Doing so taught me to plan, build, and
+          automate tools which eased my workload in the
+          transportation/logistics/freight shipping industries.
+        </p>
+        <p>
+          When I was laid off in December 2020, I decided to enrolled at the{" "}
+          <a
+            href="https://nashvillesoftwareschool.com/"
+            aria-label="Nashville Software School"
+          >
+            Nashville Software School
+          </a>
+          . NSS reinforced my skillset and taught me to use the latest
+          technologies.
+        </p>
+        <p>
+          In my free time I like to catch up on (computational) linguistics,
+          explore, and volunteer.
+        </p>
+        <p>I look forward to collaborating with you on future endeavors.</p>
+      </section>
     </Layout>
   );
 };
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,14 +1,20 @@
 import * as React from "react";
-import Layout from "../../components/layout";
-
 import { useForm, ValidationError } from "@formspree/react";
 
-import styles from "./contact.module.css";
+import Layout from "../../components/layout";
+// import styles from "./contact.module.css";
+
+const fsURL = process.env.GATSBY_FORMSPREE_URL;
+const fsEndpoint = process.env.GATSBY_FORMSPREE_ENDPOINT;
 
 const ContactPage = () => {
-  const [state, handleSubmit] = useForm("mzbyerkv");
+  const [state, handleSubmit] = useForm(fsEndpoint);
   if (state.succeeded) {
-    return <p>Thanks you for your request! </p>;
+    return (
+      <Layout pageTitle="Contact">
+        <p>Thanks you for your request!</p>
+      </Layout>
+    );
   }
 
   return (
@@ -19,30 +25,45 @@ const ContactPage = () => {
           className="main__form"
           id="fs-frm"
           name="simple-contact-form"
-          accept-charset="utf-8"
-          action="https://formspree.io/f/mzbyerkv"
+          acceptCharset="utf-8"
+          action={`${fsURL}${fsEndpoint}`}
           method="POST"
           onSubmit={handleSubmit}
         >
           <fieldset className="main__form__fieldset">
-            <label for="inputName">name</label>
-            <input name="inputName" id="inputName" type="text" />
+            <label htmlFor="inputName">name</label>
+            <input
+              name="inputName"
+              id="inputName"
+              type="text"
+              placeholder="First and Last"
+            />
             <ValidationError
               prefix="Name"
               field="inputName"
               errors={state.errors}
             />
 
-            <label for="inputEmail">email</label>
-            <input name="inputEmail" id="inputEmail" type="email" />
+            <label htmlFor="inputEmail">email</label>
+            <input
+              name="inputEmail"
+              id="inputEmail"
+              type="email"
+              placeholder="name@domain.com"
+            />
             <ValidationError
               prefix="Email"
               field="email"
               errors={state.errors}
             />
 
-            <label for="inputMsg">message</label>
-            <textarea name="inputMsg" id="inputMsg" required=""></textarea>
+            <label htmlFor="inputMsg">message</label>
+            <textarea
+              name="inputMsg"
+              id="inputMsg"
+              required=""
+              placeholder="Message"
+            ></textarea>
             <ValidationError
               prefix="Message"
               field="inputMsg"
@@ -50,12 +71,12 @@ const ContactPage = () => {
             />
 
             <input
-              type="hidden"
-              name="_subject"
               id="email-subject"
+              type="text"
+              name="_gotcha"
               value="Contact Form Submission"
+              className="moocow"
             />
-            <input type="text" name="_gotcha" className="moocow" />
           </fieldset>
           <button className="submit" type="submit" disabled={state.submitting}>
             Submit
@@ -67,34 +88,3 @@ const ContactPage = () => {
 };
 
 export default ContactPage;
-
-//   <form
-//   >
-//   <fieldset id="fs-frm-inputs">
-//     <label for="full-name">Full Name</label>
-//     <input type="text" name="name" id="full-name" placeholder="First and Last" required="">
-
-//     <label for="email-address">Email Address</label>
-//     <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="">
-//     <label for="message">Message</label>
-//     <textarea rows="5" name="message" id="message" placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus." required=""></textarea>
-//     <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
-//   </fieldset>
-//   <input type="submit" value="Submit">
-// </form>
-
-//         <form
-
-//   >
-//     <label htmlFor="email">Email Address</label>
-//     <input id="email" type="email" name="email" />
-//     <ValidationError prefix="Email" field="email" errors={state.errors} />
-
-//     <textarea id="message" name="message" />
-//     <ValidationError
-//       prefix="Message"
-//       field="message"
-//       errors={state.errors}
-//     />
-
-//   </form>
