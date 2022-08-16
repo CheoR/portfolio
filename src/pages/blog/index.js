67676990907ae2658624af1a6f2aa685e048ea55
CheoR <cheo.roman+github@gmail.com>
2022-08-16 15:41:52 -0500

    ref: move layout into own component

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Link, graphql } from "gatsby";
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout/layout";
 
 const Blog = ({ data }) => {
   return (
