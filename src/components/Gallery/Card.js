808c61f861f2144150024d71a808a954e68c99ac
CheoR <cheo.roman+github@gmail.com>
2022-08-23 09:42:33 -0500

    css: de-colorize cards

--- src/components/Gallery/Card.js
+++ src/components/Gallery/Card.js
@@ -6,7 +6,7 @@ const Card = (prop) => {
     <div key={prop.key} className={styles.card}>
       <div className={styles.card__image}>img</div>
       <div className={styles.card__chips}>chips</div>
-      <div>{prop.data.description}</div>
+      <div className={styles.card__description}>{prop.data.description}</div>
       <div className={styles.card__btns}>
         <button className={styles.card__btn}>demo</button>
         <button className={styles.card__btn}>github</button>
