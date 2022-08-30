c7e245cd5e9d1f3fdf9dc04cf35afc8610c1a1a8
CheoR <cheo.roman+github@gmail.com>
2022-08-30 15:54:41 -0500

    doc: add info

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,8 @@ module.exports = {
   siteMetadata: {
     title: `Cheo Roman Portfolio`,
     siteUrl: `https://cheor.github.io/portfolio/`,
+    author: "CheoR",
+    description: "CheoR Web Dev Portfolio",
   },
   plugins: [
     "gatsby-plugin-image",
