883c451cecd8c6f48cb78fba74b1b449f67fc4bc
CheoR <cheo.roman+github@gmail.com>
2022-09-01 15:02:29 -0500

    ref: remove MUI to own branch for testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,5 @@ module.exports = {
       },
       __key: "images",
     },
-    "gatsby-theme-material-ui",
   ],
 };
--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -21,20 +21,11 @@ export const SEO = ({ title, description, image, pathname, children }) => {
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
       <meta property="og:title" content={seo.title} />
       <meta property="og:url" content={seo.url} />
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
       <meta property="og:type" content="website" />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      />
       <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>👤</text></svg>"
