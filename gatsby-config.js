c6f9c6403c0e9e8de40c19395dd1daff3b011feb
CheoR <cheo.roman+github@gmail.com>
2022-09-01 13:52:09 -0500

    doc: add MUI

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,5 +46,6 @@ module.exports = {
       },
       __key: "images",
     },
+    "gatsby-theme-material-ui",
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+
 import Layout from "../components/Layout/Layout";
 import Gallery from "../components/Gallery/Gallery";
 import { SEO } from "../components/SEO/SEO";
