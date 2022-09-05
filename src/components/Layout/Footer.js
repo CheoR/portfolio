ae24e6893c2f873e376384f0d8b3681d79b48b06
CheoR <cheo.roman+github@gmail.com>
2022-09-05 08:34:37 -0500

    fix: flex social icons

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -21,7 +21,6 @@ const Footer = () => {
             variant="h6"
             noWrap
             sx={{
-              // display: { xs: "flex", md: "none" },
               flexGrow: 1,
               fontFamily: "monospace",
               fontWeight: 400,
@@ -29,44 +28,33 @@ const Footer = () => {
               color: "inherit",
               textDecoration: "none",
               textAlign: "center",
-              background: "pink",
             }}
           >
             Let's Connect!
           </Typography>
         </Grid>
         <Grid item xs={12} md={8} order={{ xs: 1, md: 2 }}>
-          <Box>
-            <Typography
-              variant="h6"
-              noWrap
-              sx={{
-                // display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 400,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-                textAlign: "center",
-                background: "lightblue",
-              }}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <IconButton
+              href={SOCIALS.linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <IconButton
-                href={SOCIALS.linkedin}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <LinkedInIcon fontSize="large" />
-              </IconButton>
-              <IconButton
-                href={SOCIALS.github}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GitHubIcon fontSize="large" />
-              </IconButton>
-            </Typography>
+              <LinkedInIcon fontSize="large" />
+            </IconButton>
+            <IconButton
+              href={SOCIALS.github}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitHubIcon fontSize="large" />
+            </IconButton>
           </Box>
         </Grid>
         <Grid item xs={12} md={4} order={{ xs: 2, md: 1 }}>
@@ -74,15 +62,16 @@ const Footer = () => {
             variant="h6"
             noWrap
             sx={{
-              // display: { xs: "flex", md: "none" },
               flexGrow: 1,
               fontFamily: "monospace",
               fontWeight: 400,
               letterSpacing: ".3rem",
               color: "inherit",
               textDecoration: "none",
-              textAlign: "center",
-              background: "lightblue",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
             &copy; CheoR {new Date().getFullYear().toString()}
