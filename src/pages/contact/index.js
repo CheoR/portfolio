075937bf2e5d8be1b8a5e017a6d3a470a09a3679
CheoR <cheo.roman+github@gmail.com>
2022-09-08 10:02:13 -0500

    fix: center contact form

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useForm, ValidationError } from "@formspree/react";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import Layout from "../../components/Layout/Layout";
 import { SEO } from "../../components/SEO/SEO";
@@ -21,84 +22,87 @@ const ContactPage = () => {
   }
 
   return (
-    <Layout pageTitle="Contact">
-      <Box sx={{ height: "100vh" }}>
-        <form
-          className={styles.main__form}
-          id="fs-frm"
-          name="simple-contact-form"
-          acceptCharset="utf-8"
-          action={`${fsURL}${fsEndpoint}`}
-          method="POST"
-          onSubmit={handleSubmit}
-        >
-          <fieldset className={styles.main__form__fieldset}>
-            <label className={styles.main__form_label} htmlFor="inputName">
-              name
-            </label>
-            <input
-              className={styles.main__form_input}
-              name="inputName"
-              id="inputName"
-              type="text"
-              placeholder="First and Last"
-            />
-            <ValidationError
-              prefix="Name"
-              field="inputName"
-              errors={state.errors}
-            />
-
-            <label className={styles.main__form_label} htmlFor="inputEmail">
-              email
-            </label>
-            <input
-              className={styles.main__form_input}
-              name="inputEmail"
-              id="inputEmail"
-              type="email"
-              placeholder="name@domain.com"
-            />
-            <ValidationError
-              prefix="Email"
-              field="email"
-              errors={state.errors}
-            />
-
-            <label className={styles.main__form_label} htmlFor="inputMsg">
-              message
-            </label>
-            <textarea
-              className={styles.main__form_textarea}
-              name="inputMsg"
-              id="inputMsg"
-              required=""
-              placeholder="Message"
-            ></textarea>
-            <ValidationError
-              prefix="Message"
-              field="inputMsg"
-              errors={state.errors}
-            />
-
-            <input
-              id="email-subject"
-              type="text"
-              name="_gotcha"
-              className={styles.moocow}
-            />
-          </fieldset>
-
-          <button
-            className={styles.main__form_submit}
-            type="submit"
-            disabled={state.submitting}
+    <>
+      <CssBaseline />
+      <Layout pageTitle="Contact">
+        <Box sx={{ height: "100%", display: "flex", justifyContent: "center" }}>
+          <form
+            className={styles.main__form}
+            id="fs-frm"
+            name="simple-contact-form"
+            acceptCharset="utf-8"
+            action={`${fsURL}${fsEndpoint}`}
+            method="POST"
+            onSubmit={handleSubmit}
           >
-            Submit
-          </button>
-        </form>
-      </Box>
-    </Layout>
+            <fieldset className={styles.main__form__fieldset}>
+              <label className={styles.main__form_label} htmlFor="inputName">
+                name
+              </label>
+              <input
+                className={styles.main__form_input}
+                name="inputName"
+                id="inputName"
+                type="text"
+                placeholder="First and Last"
+              />
+              <ValidationError
+                prefix="Name"
+                field="inputName"
+                errors={state.errors}
+              />
+
+              <label className={styles.main__form_label} htmlFor="inputEmail">
+                email
+              </label>
+              <input
+                className={styles.main__form_input}
+                name="inputEmail"
+                id="inputEmail"
+                type="email"
+                placeholder="name@domain.com"
+              />
+              <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+              />
+
+              <label className={styles.main__form_label} htmlFor="inputMsg">
+                message
+              </label>
+              <textarea
+                className={styles.main__form_textarea}
+                name="inputMsg"
+                id="inputMsg"
+                required=""
+                placeholder="Message"
+              ></textarea>
+              <ValidationError
+                prefix="Message"
+                field="inputMsg"
+                errors={state.errors}
+              />
+
+              <input
+                id="email-subject"
+                type="text"
+                name="_gotcha"
+                className={styles.moocow}
+              />
+            </fieldset>
+
+            <button
+              className={styles.main__form_submit}
+              type="submit"
+              disabled={state.submitting}
+            >
+              Submit
+            </button>
+          </form>
+        </Box>
+      </Layout>
+    </>
   );
 };
 
